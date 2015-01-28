b43582f90e9c928cf2404817c23743d38fa0e168
Shane Rogers <rogerssh@tcd.ie>
2015-01-27 23:21:01 -0800

    Added video playback to the cells

--- SDRKamkordViewer/SDRVideoFeedViewController.m
+++ SDRKamkordViewer/SDRVideoFeedViewController.m
@@ -9,6 +9,7 @@
 #import "SDRVideoFeedViewController.h"
 
 #import <SDWebImage/UIImageView+WebCache.h>
+#import <MediaPlayer/MediaPlayer.h>
 
 // Components
 #import "SDRVideoFeedTableViewCell.h"
@@ -24,8 +25,9 @@
 @interface SDRVideoFeedViewController ()
 
 @property (nonatomic, strong) UITableView *videoFeed;
-@property (nonatomic, strong) SDRVideoChannel *_videoChannel;
-@property (nonatomic, strong) SDRSpinner *_spinner;
+@property (nonatomic, strong) SDRVideoChannel *videoChannel;
+@property (nonatomic, strong) SDRSpinner *spinner;
+@property (nonatomic, strong) MPMoviePlayerController *videoPlayer;
 
 @end
 
@@ -35,7 +37,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     [self addVideosTable];
-    self._spinner = [SDRSpinner new];
+    self.spinner = [SDRSpinner new];
     [self fetchVideos];
 }
 
@@ -44,7 +46,7 @@
 
     void(^completionBlock)(SDRVideoChannel *obj, NSError *err)=^(SDRVideoChannel *obj, NSError *err){
         if(!err){
-            self._videoChannel = obj;
+            self.videoChannel = obj;
             [self.videoFeed reloadData];
         } else {
             [self renderError:err];
@@ -66,21 +68,22 @@
 
 - (void)showOrHideSpinner:(BOOL)value {
     if(value) {
-        [self._spinner setSpinnerImagesWithView:self.view];
+        [self.spinner setSpinnerImagesWithView:self.view];
 
-        [self._spinner startAnimating];
-        [self.view addSubview:self._spinner];
+        [self.spinner startAnimating];
+        [self.view addSubview:self.spinner];
     } else {
-        [self._spinner stopAnimating];
-        [self._spinner removeFromSuperview];
+        [self.spinner stopAnimating];
+        [self.spinner removeFromSuperview];
     }
 }
 
 - (void)addVideosTable {
     self.videoFeed = [[UITableView alloc] initWithFrame:self.view.frame style:UITableViewStylePlain];
+    [self.videoFeed setBackgroundColor:[UIColor clearColor]];
     [self.videoFeed setSeparatorColor:[UIColor clearColor]];
     self.videoFeed.backgroundView = [[UIView alloc] initWithFrame:self.view.frame];
-    [self.videoFeed.backgroundView setBackgroundColor:[UIColor grayColor]];
+    [self.videoFeed.backgroundView setBackgroundColor:[UIColor colorWithPatternImage:[UIImage imageNamed:@"launch_screen"]]];
 
     [self.videoFeed registerClass:[UITableViewCell class] forCellReuseIdentifier:kSDRVideoFeedTableViewCell];
     self.videoFeed.delegate = self;
@@ -95,18 +98,13 @@
     [self.view addSubview:self.videoFeed];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
 #pragma UITableViewDelgate
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
     return 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return [self._videoChannel.videos count];
+    return [self.videoChannel.videos count];
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
@@ -114,7 +112,7 @@
     SDRVideoFeedTableViewCell *cell = [nibContents lastObject];
 
     if([tableView isEqual:self.videoFeed]) {
-        SDRVideo *video = [self._videoChannel.videos objectAtIndex:[indexPath row]];
+        SDRVideo *video = [self.videoChannel.videos objectAtIndex:[indexPath row]];
 
         [cell.videoTitle setText:video.title];
         [cell.videoThumbnail sd_setImageWithURL:[NSURL URLWithString:video.thumbnailUrl] placeholderImage:nil];
@@ -122,8 +120,75 @@
     return cell;
 }
 
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    SDRVideo *video = [self.videoChannel.videos objectAtIndex:[indexPath row]];
+
+    if (self.videoPlayer != nil){
+        [self stopPlayingVideo:nil];
+    }
+
+    MPMoviePlayerController *videoPlayer =[[MPMoviePlayerController alloc] initWithContentURL:[NSURL URLWithString:video.videoUrl]];
+    self.videoPlayer = videoPlayer;
+
+    if (self.videoPlayer != nil){
+        [[NSNotificationCenter defaultCenter] addObserver:self
+                                                 selector:@selector(videoHasFinishedPlaying:)
+                                                     name:MPMoviePlayerPlaybackDidFinishNotification
+                                                   object:self.videoPlayer];
+
+        self.videoPlayer.scalingMode = MPMovieScalingModeAspectFit;
+        [self.view addSubview:self.videoPlayer.view];
+        [self.videoPlayer setFullscreen:YES
+                               animated:NO];
+
+        [self.videoPlayer play];
+    } else {
+        NSLog(@"Failed to initalize the video player");
+    }
+}
+
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
     return kSDRVideoFeedCellHeight;
+}
+
+#pragma VideoPlayer methods
+- (void)stopPlayingVideo:(id)paramSender {
+    if (self.videoPlayer != nil) {
+        [[NSNotificationCenter defaultCenter] removeObserver:self
+                                                        name:MPMoviePlayerPlaybackDidFinishNotification
+                                                      object:self.videoPlayer];
+
+        [self.videoPlayer stop];
+        [self.videoPlayer.view removeFromSuperview];
+    }
+}
+
+- (void)videoHasFinishedPlaying:(NSNotification *)paramNotification {
+    NSNumber *reason = paramNotification.userInfo[MPMoviePlayerPlaybackDidFinishReasonUserInfoKey];
+
+    if (reason != nil) {
+        NSInteger reasonAsInteger = [reason integerValue];
+
+        switch (reasonAsInteger){
+            case MPMovieFinishReasonPlaybackEnded:{
+                NSLog(@"MPMovieFinishReasonPlaybackEnded");
+                break; }
+            case MPMovieFinishReasonPlaybackError:{
+                NSLog(@"MPMovieFinishReasonPlaybackError");
+                break;
+            }
+            case MPMovieFinishReasonUserExited:{
+                NSLog(@"MPMovieFinishReasonUserExited");
+                break;
+            }
+        }
+        [self stopPlayingVideo:nil];
+    }
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
 }
 
 @end
